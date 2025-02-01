16936e386ed449e36f38b76ab02a341a93f9eb57
Jan Bohacek <jan.bohacek.91@gmail.com>
2025-02-01 21:53:52 +0100

    First version of working sort

--- scripts/mergeSort.py
+++ scripts/mergeSort.py
@@ -1,10 +1,25 @@
 from merge import merge
 
-a = [5,4,9,6,2,3,9,3,7]
+input = [5,4,9,6,2,3,9,3,7,16,1,1965,12,6,0,48]
 
-l = [4,5,6,9]
-r = [2,3,3,7,9]
+def mergeSort(in_array, a=0, b=None):
+    if not b:
+        b = len(in_array)
 
-print(f"Original Array: {a}")
-merge(a, l, r)
-print(f"Sorted Array: {a}")
+    c = b // 2
+    l,r = in_array[a:c], in_array[c:b]
+    # print(l, r)
+    if len(l) > 1:
+        mergeSort(l, a, c)
+    if len(r) > 1:
+        mergeSort(r, a, b-c)
+    # print(l,r)
+    merge(in_array, l, r)
+
+mergeSort(input)
+
+
+
+# print(f"Original Array: {a}")
+# merge(a, l, r)
+# print(f"Sorted Array: {a}")
